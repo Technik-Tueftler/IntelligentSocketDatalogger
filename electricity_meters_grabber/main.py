316defa59ebd136d07fe23ba3d222a15977c1513
Technik Tüftler <technik.tueftler@gmail.com>
2022-11-09 23:28:15 +0100

    implement IR breakout

--- electricity_meters_grabber/main.py
+++ electricity_meters_grabber/main.py
@@ -2,7 +2,7 @@
 
 import time
 import urequests
-import ntptime
+from machine import Pin, ADC
 
 print("Start main programm")
 
@@ -11,6 +11,8 @@ time.sleep(2)
 print(time.time())
 time.sleep(2)
 print(time.localtime())
+import ntptime
+
 ntptime.settime()
 now = time.localtime()
 print(now)
@@ -19,3 +21,12 @@ print(time.mktime(now))
 data = 'census,device=server01 power=0.99,device_temperature=9.9'
 response = urequests.post("http://192.168.178.39:8086/write?db=test", data=data)
 print(response)
+
+led = Pin(22, Pin.IN)
+# dis = ADC(Pin(34))
+# dis.atten(ADC.ATTN_11DB)
+
+while True:
+    print(led.value())
+    # print(dis.read())
+    time.sleep(0.2)
